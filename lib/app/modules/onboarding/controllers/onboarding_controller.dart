25ac48938e4169bd2e6f00118febbea553db01c5
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-19 16:11:36 +0700

    feat: add onboarding page navigation

--- lib/app/modules/onboarding/controllers/onboarding_controller.dart
+++ lib/app/modules/onboarding/controllers/onboarding_controller.dart
@@ -5,23 +5,35 @@ class OnboardingController extends GetxController {
   final PageController pageController = PageController();
 
   final currentPage = 0.obs;
+  final totalPages = 5;
 
   void onPageChanged(int index) {
     currentPage.value = index;
   }
 
   void nextPage() {
-    pageController.nextPage(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+    if (currentPage.value < totalPages - 1) {
+      pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      completeOnboarding();
+    }
   }
 
   void previousPage() {
-    pageController.previousPage(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+    if (currentPage.value > 0) {
+      pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void completeOnboarding() {
+    // Menyimpan jawaban onboarding.
+    // Mengarahkan user ke Register page.
   }
 
   @override
