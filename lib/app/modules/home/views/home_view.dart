42a397fa1c202696cc5e627524c7287d0120d738
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-21 07:38:08 +0700

    feat: integrate AppMainHeader into home page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
+import '../../../theme/app_colors.dart';
+import '../../../theme/app_spacing.dart';
+import '../../../widgets/app_main_header.dart';
+import '../../../widgets/app_stat_chip.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: Column(
+          children: [
+            AppMainHeader(
+              title: 'Home',
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppSpacing.xl,
+                vertical: AppSpacing.md,
+              ),
+              actions: [
+                Obx(
+                  () => AppStatChip(
+                    icon: const Icon(
+                      Icons.event_available_rounded,
+                      size: 18,
+                      color: AppColors.blue500,
+                    ),
+                    value: '${controller.studyProgress.value}',
+                    tooltip: 'Progres Belajar',
+                    backgroundColor: AppColors.blue50,
+                    borderColor: AppColors.blue100,
+                    textColor: AppColors.blue500,
+                    onTap: () {
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'HomeView is working',
+                  style: TextStyle(fontSize: 20, color: AppColors.textMedium),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
