b36e48ca1559f7e38516d1e9dfe39dc321b32f37
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-21 19:48:24 +0700

    feat: add simulation data to lab controller

--- lib/app/modules/lab/controllers/lab_controller.dart
+++ lib/app/modules/lab/controllers/lab_controller.dart
@@ -1,5 +1,23 @@
 import 'package:get/get.dart';
 
+import '../data/lab_simulations.dart';
+import '../models/simulation_model.dart';
+
 class LabController extends GetxController {
   final studyProgress = 0.obs;
+  final simulations = <SimulationModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadSimulations();
+  }
+
+  void loadSimulations() {
+    simulations.assignAll(LabSimulations.items);
+  }
+
+  void onSimulationSelected(SimulationModel simulation) {
+    // Navigasi ke halaman simulasi spesifik
+  }
 }
