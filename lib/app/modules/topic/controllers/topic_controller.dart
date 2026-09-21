8874a508a918d4e2007b9a5fa0ecc149cebc1777
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-21 15:42:33 +0700

    feat: handle topic activity selection

--- lib/app/modules/topic/controllers/topic_controller.dart
+++ lib/app/modules/topic/controllers/topic_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 import '../../home/models/topic_model.dart';
+import '../models/topic_activity_type.dart';
 
 class TopicController extends GetxController {
   final topic = Rxn<TopicModel>();
@@ -8,8 +9,13 @@ class TopicController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+
     if (Get.arguments is TopicModel) {
       topic.value = Get.arguments as TopicModel;
     }
+  }
+
+  void onActivitySelected(TopicActivityType activity) {
+    // Mengarahkan ke halaman aktivitas yang dipilih
   }
 }
