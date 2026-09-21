7cb07bd2d1b8b38143e2cbfdcb528cb68f1e2bcf
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-21 11:12:25 +0700

    feat: add topic management to home controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,23 @@
 import 'package:get/get.dart';
 
+import '../data/home_topics.dart';
+import '../models/topic_model.dart';
+
 class HomeController extends GetxController {
   final studyProgress = 0.obs;
+  final topics = <TopicModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadTopics();
+  }
+
+  void loadTopics() {
+    topics.assignAll(HomeTopics.items);
+  }
+
+  void onTopicSelected(TopicModel topic) {
+    // Aksi ketika topik dipilih
+  }
 }
