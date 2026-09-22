470dbc3d447f48e5ab83edeb9e582bbb034d0848
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-22 11:20:44 +0700

    feat: add flashcard page header

--- lib/app/modules/flashcard/views/flashcard_view.dart
+++ lib/app/modules/flashcard/views/flashcard_view.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
+import '../../../theme/app_colors.dart';
+import '../../../widgets/app_step_header.dart';
 import '../controllers/flashcard_controller.dart';
 
 class FlashcardView extends GetView<FlashcardController> {
   const FlashcardView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlashcardView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'FlashcardView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            AppStepHeader.steps(
+              currentStep: 1,
+              totalSteps: 10,
+              backIcon: Icons.close_rounded,
+              progressColor: AppColors.blue500,
+              trackColor: AppColors.border,
+              onBackPressed: () => Get.back(),
+            ),
+            const Expanded(child: SizedBox(width: double.infinity)),
+          ],
         ),
       ),
     );
