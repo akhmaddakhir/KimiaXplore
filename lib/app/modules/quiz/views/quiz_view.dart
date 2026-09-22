f9a028c771de35deba877b7de399cb76897cb12d
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-22 07:12:15 +0700

    feat: integrate quiz progress header

--- lib/app/modules/quiz/views/quiz_view.dart
+++ lib/app/modules/quiz/views/quiz_view.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+
+import '../../../theme/app_colors.dart';
+import '../../../widgets/app_step_header.dart';
 
 import '../controllers/quiz_controller.dart';
 
 class QuizView extends GetView<QuizController> {
   const QuizView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QuizView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'QuizView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: AppColors.background,
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            Obx(
+              () => AppStepHeader.steps(
+                currentStep: controller.currentQuestionNumber,
+                totalSteps: controller.totalQuestions,
+                backIcon: Icons.close_rounded,
+                progressColor: AppColors.blue500,
+                trackColor: AppColors.border,
+                onBackPressed: () => Get.back(),
+              ),
+            ),
+
+            const Expanded(child: SizedBox.shrink()),
+          ],
         ),
       ),
     );
