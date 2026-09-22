cf64fc22025c5488f230043e61072888ae452d7b
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-09-22 11:26:56 +0700

    feat: integrate memorization buttons into flashcard view

--- lib/app/modules/flashcard/views/flashcard_view.dart
+++ lib/app/modules/flashcard/views/flashcard_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import '../../../theme/app_colors.dart';
 import '../../../widgets/app_step_header.dart';
 import '../controllers/flashcard_controller.dart';
+import '../widgets/flashcard_bottom_bar.dart';
 
 class FlashcardView extends GetView<FlashcardController> {
   const FlashcardView({super.key});
@@ -25,6 +26,7 @@ class FlashcardView extends GetView<FlashcardController> {
               onBackPressed: () => Get.back(),
             ),
             const Expanded(child: SizedBox(width: double.infinity)),
+            FlashcardBottomBar(onNotMemorized: () {}, onMemorized: () {}),
           ],
         ),
       ),
